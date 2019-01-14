630cf456daad996b2a1f888ca8a334ba98bd0164
godsby <gg4biz@gmail.com>
2019-01-14 14:37:29 -0500

    Finally!!!

--- src/form.js
+++ src/form.js
@@ -19,6 +19,21 @@ class MarsForm extends React.Component{
       marital: '',
       stress_handling: '',
       claustrophobic: '',
+      familyHistory: {
+        cancer: false,
+        heartDisease: false,
+        diabetes: false
+      },
+      family: {
+        siblings: false,
+        parents: false,
+        grandparents: false
+      },
+      familyMembers: {
+        siblings: '',
+        parents: '',
+        grandparents: ''
+      },
       educations: {
         high_school: false,
         associate: false,
@@ -40,6 +55,7 @@ class MarsForm extends React.Component{
     this.handleComplete = this.handleComplete.bind(this);
     this.handleRadioChange = this.handleRadioChange.bind(this);
     this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
+    this.handleSelectBoxes = this.handleSelectBoxes.bind(this);
   }
 
   handleChange = (event) => {
@@ -69,16 +85,55 @@ class MarsForm extends React.Component{
   }
   
   handleCheckboxChange = (event) => {
-    const {educations, educationValues} = this.state;
-    let newEducations = {...educations};
-    newEducations[event.target.id] = !newEducations[event.target.id];
-    let newEducationValues = {...educationValues};
-    newEducationValues[event.target.id] = newEducations[event.target.id] ? event.target.id : '';
+ 
+    const {familyHistory, family, educations, educationValues} = this.state;
+    const {name, id} = event.target;
 
-    this.setState({
-      educations: newEducations,
-      educationValues: newEducationValues 
-    })
+    if (id === 'familyHistory') {
+      let newFamilyHistroy = {...familyHistory};
+      newFamilyHistroy[name] = !newFamilyHistroy[name];
+      this.setState({
+        familyHistory: newFamilyHistroy
+      })
+    } else if (id === 'family') {
+      event.stopPropagation();
+      let newFamily = {...family};
+      newFamily[name] = !newFamily[name];
+      // let newFamilyMembers = {...familyMembers};
+      // newFamilyMembers[name] = newFamily[name] ? name : '';
+      this.setState({
+        family: newFamily,
+        // familyMembers: newFamilyMembers
+      })
+    } else if (id === 'educationBackground') {
+      event.stopPropagation();
+      let newEducations = {...educations};
+      newEducations[name] = !newEducations[name];
+      let newEducationValues = {...educationValues};
+      newEducationValues[name] = newEducations[name] ? name : '';
+      this.setState({
+        educations: newEducations,
+        educationValues: newEducationValues
+      })
+    } else {
+      event.preventDefault();
+    } 
+  }
+
+  handleSelectBoxes =(event) => {
+    event.stopPropagation();
+    const {family, familyMembers} = this.state;
+    const {name, value} = event.target;
+    debugger
+    if (family.siblings || family.parents || family.grandparents) {
+      let newFamilyMembers = {...familyMembers};
+      newFamilyMembers[name] = value;
+      this.setState({
+        familyMembers: newFamilyMembers
+      })
+    } else {
+      event.preventDefault();
+    }
   }
 
   radioQuestions () {
@@ -208,64 +263,165 @@ class MarsForm extends React.Component{
   }
 
   checkboxQuestions () {
-    const {educations, educationValues, otherEdu} = this.state;
+    const {familyHistory, family, familyMembers, educations, educationValues, otherEdu} = this.state;
+    const siblingsSelect = family.siblings ? 
+      <select id='family' name='siblings' value={familyMembers.siblings} onChange={this.handleSelectBoxes}>
+        <option value=''>---How Many?---</option>
+        <option value='1'>1</option>
+        <option value='2'>2</option>
+        <option value='3'>3</option>
+        <option value='4'>4</option>
+        <option value='5'>5</option>
+      </select> : null;
+    const parentsSelect = family.parents ? 
+      <select id='family' name='parents' value={familyMembers.parents} onChange={this.handleSelectBoxes}>
+        <option value=''>---How Many?---</option>
+        <option value='1'>1</option>
+        <option value='2'>2</option>
+        <option value='3'>3</option>
+        <option value='4'>4</option>
+        <option value='5'>5</option>
+      </select> : null;
+    const grandparentsSelect = family.grandparents ? 
+      <select id='family' name='grandparents' value={familyMembers.grandparents} onChange={this.handleSelectBoxes}>
+        <option value=''>---How Many?---</option>
+        <option value='1'>1</option>
+        <option value='2'>2</option>
+        <option value='3'>3</option>
+        <option value='4'>4</option>
+        <option value='5'>5</option>
+      </select> : null;
 
     return (
       <>
-        <p><b>Check all educational credentials you have received:</b></p>
+        <p><b>Does your family have a history of(check all that apply):</b></p>
+        
+        <div onChange={this.handleCheckboxChange}>
+          <label>
+            <input 
+              type='checkbox'
+              id='familyHistory'
+              name='cancer'
+              checked={familyHistory.name}
+            /> Cancer
+          </label><br />
+        
+          <label>
+            <input 
+              type='checkbox'
+              id='familyHistory'
+              name='heartDisease'
+              checked={familyHistory.name}
+            /> Heart Disease
+          </label><br />
+          
+          <label>
+            <input 
+              type='checkbox'
+              id='familyHistory'
+              name='diabetes'
+              checked={familyHistory.name}
+            /> Diabetes
+          </label><br />
+        </div>
+        
+        <p><b>Do you have any living(check all that apply):</b></p>
 
-        <label>
-          <input 
-            onChange={this.handleCheckboxChange} 
-            type='checkbox' 
-            checked={educations.id} 
-            value={educationValues.id} 
-            id='high_school'/> High school diploma or GED equivalent
-        </label><br />
-
-        <label>
-          <input 
-            onChange={this.handleCheckboxChange} 
-            type='checkbox' 
-            checked={educations.id}
-            value={educationValues.id} 
-            id='associate'/> Associate Degree
-        </label><br />
-    
-        <label >
-          <input 
-            onChange={this.handleCheckboxChange} 
-            type='checkbox' 
-            checked={educations.id}
-            value={educationValues.id} 
-            id='bachelor'/> Bachelor Degree
-        </label><br />
-
-        <label >
-          <input 
-            onChange={this.handleCheckboxChange} 
-            type='checkbox' 
-            checked={educations.id} 
-            value={educationValues.id} 
-            id='master'/> Master Degree
-        </label><br />
+        <div>
+          <label>
+            <input 
+              type='checkbox'
+              id='family'
+              name='siblings'
+              checked={family.name}
+              onChange={this.handleCheckboxChange}
+            /> Siblings
+            {siblingsSelect}
+          </label><br />
       
-        <label>
-          <input 
-            onChange={this.handleCheckboxChange} 
-            type='checkbox' 
-            checked={educations.id} 
-            value={educationValues.id} 
-            id='PhD'/> PhD
-        </label><br /><br />
+          <label>
+            <input 
+              type='checkbox'
+              id='family'
+              name='parents'
+              checked={family.name}
+              onChange={this.handleCheckboxChange}
+            /> Parents
+            {parentsSelect}
+          </label><br />
+          
+          <label>
+            <input 
+              type='checkbox'
+              id='family'
+              name='grandparents'
+              checked={family.name}
+              onChange={this.handleCheckboxChange}
+            /> Grandparents
+            {grandparentsSelect}
+          </label><br />
+        </div>
+        
+        <p><b>Check all educational credentials you have received:</b></p>
+        <div onChange={this.handleCheckboxChange}>
+          <label>
+            <input 
+              type='checkbox' 
+              id='educationBackground'
+              name='high_school'
+              checked={educations.name} 
+              value={educationValues.name} 
+            /> High school diploma or GED equivalent
+          </label><br />
 
-        <label> Other<br />
-          <input 
-            type='text' 
-            name='otherEdu' 
-            value={otherEdu} id='otherEdu' 
-            placeholder='Please typein'/>
-        </label><br />
+          <label>
+            <input 
+              type='checkbox'
+              id='educationBackground' 
+              name='associate'
+              checked={educations.name}
+              value={educationValues.name} 
+            /> Associate Degree
+          </label><br />
+    
+          <label >
+            <input 
+              type='checkbox' 
+              id='educationBackground'
+              name='bachelor'
+              checked={educations.name}
+              value={educationValues.name} 
+            /> Bachelor Degree
+          </label><br />
+
+          <label >
+            <input 
+              type='checkbox' 
+              id='educationBackground'
+              name='master'
+              checked={educations.name} 
+              value={educationValues.name} 
+            /> Master Degree
+          </label><br />
+      
+          <label>
+            <input 
+              type='checkbox' 
+              id='educationBackground'
+              name='PhD'
+              checked={educations.name} 
+              value={educationValues.name} 
+            /> PhD
+          </label><br /><br />
+
+          <label> Other<br />
+            <input 
+              type='text' 
+              name='otherEdu' 
+              value={otherEdu} 
+              placeholder='Please typein'/>
+          </label><br />
+        </div>
       </>
     )
   }
@@ -273,7 +429,7 @@ class MarsForm extends React.Component{
   render() {
     console.log(this.state);
 
-    const {firstname, lastname, dob, countries, country, dietary, reason, formCompleted, formSubmitted, breathe, marital, stress_handling, claustrophobic, educations, educationValues, otherEdu} = this.state;
+    const {firstname, lastname, dob, countries, country, dietary, reason, formCompleted, formSubmitted, breathe, marital, stress_handling, claustrophobic, familyHistory, family, familyMembers, educations, educationValues, otherEdu} = this.state;
     
     let options = countries.map(country => {
       return <option>{country.name}</option>
@@ -323,7 +479,7 @@ class MarsForm extends React.Component{
             <button 
               className='submit'
               disabled={
-                !(firstname && lastname && dob && country && dietary && reason && breathe && marital && stress_handling && claustrophobic && (educations.high_school || educations.associate || educations.bachelor || educations.master || educations.PhD || otherEdu))
+                !(firstname && lastname && dob && country && dietary && reason && breathe && marital && stress_handling && claustrophobic && (educations.high_school || educations.associate || educations.bachelor || educations.master || educations.PhD || otherEdu) && (familyHistory.cancer || familyHistory.heartDisease || familyHistory.diabetes) && (family.siblings || family.parents || family.grandparents))
               }
               onClick={this.handleComplete}>Submit
             </button>
@@ -347,6 +503,16 @@ class MarsForm extends React.Component{
                 <li>Stress_handling: {stress_handling}</li>
                 <li>Is claustrophobic: {claustrophobic}</li>
                 <li>Eduction: {educationValues.high_school} {educationValues.associate} {educationValues.bachelor} {educationValues.master} {educationValues.PhD} {otherEdu}</li>
+                <li>Family History: 
+                  <ol>Cancer: {familyHistory.cancer ? 'Yes' : 'No'}</ol>
+                  <ol>Heart Disease: {familyHistory.heartDisease ? 'Yes' : 'No'}</ol>
+                  <ol>Diabetes: {familyHistory.diabetes ? 'Yes' : 'No'}</ol>
+                </li>
+                <li>Living Family Members: 
+                  <ol>Siblings: {family.siblings ? familyMembers.siblings : '0'}</ol>
+                  <ol>Parents: {family.parents ? familyMembers.parents : '0'}</ol>
+                  <ol>Grandparents: {family.grandparents ? familyMembers.grandparents : '0'}</ol>
+                </li>
               </p><br/>
               
               <p>Please check if all information are correct!</p><br/>
